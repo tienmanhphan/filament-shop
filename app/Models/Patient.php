88bcd9bb6dfe40d3ab1401d795a39e23bb7ae8cd
Manh Phan <manh.phan2@bnksolution.com>
2024-11-18 17:22:33 +0700

    init code

---
+++ app/Models/Patient.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Patient extends Model
+{
+    use HasFactory;
+
+    public function owner()
+    {
+        return $this->belongsTo(Owner::class);
+    }
+ 
+    public function treatments()
+    {
+        return $this->hasMany(Treatment::class);
+    }
+}
